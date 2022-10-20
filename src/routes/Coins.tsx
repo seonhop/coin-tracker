21177998ac94ee0c13fa0429e17eacbf83bf638c
seonhop <78877741+seonhop@users.noreply.github.com>
2022-10-20 01:34:09 -0400

    4.3 Home part Two
    axios -> the fetched data is json
    tell Typescript coins is list by setting useState<coinInterface[]>

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Container = styled.div`
 	padding: 0px 20px;
+	max-width: 480px;
+	margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -36,49 +40,49 @@ const Title = styled.h1`
 	font-size: 48px;
 `;
 
-const coins = [
-	{
-		id: "btc-bitcoin",
-		name: "Bitcoin",
-		symbol: "BTC",
-		rank: 1,
-		is_new: false,
-		is_active: true,
-		type: "coin",
-	},
-	{
-		id: "eth-ethereum",
-		name: "Ethereum",
-		symbol: "ETH",
-		rank: 2,
-		is_new: false,
-		is_active: true,
-		type: "coin",
-	},
-	{
-		id: "hex-hex",
-		name: "HEX",
-		symbol: "HEX",
-		rank: 3,
-		is_new: false,
-		is_active: true,
-		type: "token",
-	},
-];
+const Loader = styled.span`
+	text-align: center;
+	display: block;
+`;
+
+interface CoinInterface {
+	id: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	is_new: boolean;
+	is_active: boolean;
+	type: string;
+}
 
 function Coins() {
+	const [coins, setCoins] = useState<CoinInterface[]>([]);
+	const [loading, setLoading] = useState(true);
+	const getCoins = async () => {
+		const res = await axios("https://api.coinpaprika.com/v1/coins");
+		setCoins(res.data.slice(0, 100));
+		setLoading(false);
+	};
+	useEffect(() => {
+		getCoins();
+	}, []);
+
 	return (
 		<Container>
 			<Header>
 				<Title>Coins</Title>
 			</Header>
-			<CoinsList>
-				{coins.map((coin) => (
-					<Coin key={coin.id}>
-						<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-					</Coin>
-				))}
-			</CoinsList>
+			{loading ? (
+				<Loader>"Loading..."</Loader>
+			) : (
+				<CoinsList>
+					{coins.map((coin) => (
+						<Coin key={coin.id}>
+							<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+						</Coin>
+					))}
+				</CoinsList>
+			)}
 		</Container>
 	);
 }
