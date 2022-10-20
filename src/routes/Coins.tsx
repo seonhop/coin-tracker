03d72be807454d016145717794dcdac029a9c306
seonhop <78877741+seonhop@users.noreply.github.com>
2022-10-20 02:00:31 -0400

    4.4 Route States
    Using state from <Link to = "" state = {{}}>
    
    {state?.name || "Loading"} -> if state.name exists use state.name else use "Loading"

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -24,9 +24,10 @@ const Coin = styled.li`
 	margin-bottom: 10px;
 	border-radius: 15px;
 	a {
+		display: flex;
+		align-items: center;
 		padding: 20px;
 		transition: color 0.2s ease-in;
-		display: block;
 	}
 	&:hover {
 		a {
@@ -43,6 +44,12 @@ const Title = styled.h1`
 const Loader = styled.span`
 	text-align: center;
 	display: block;
+`;
+
+const Img = styled.img`
+	width: 35px;
+	height: 35px;
+	margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -78,7 +85,17 @@ function Coins() {
 				<CoinsList>
 					{coins.map((coin) => (
 						<Coin key={coin.id}>
-							<Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+							<Link
+								to={`/${coin.id}`}
+								state={{
+									name: coin.name,
+								}}
+							>
+								<Img
+									src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+								/>
+								{coin.name} &rarr;
+							</Link>
 						</Coin>
 					))}
 				</CoinsList>
